361674a1b1b0b4acf00510cd7063986ea8baeefe
behring <behring.zhao@gmail.com>
2022-12-29 00:32:46 +0800

    Add navigation fromr firstfragment to secondfragment.

--- app/src/main/java/cn/behring/fragmentnavigationmanagement/FirstFragment.kt
+++ app/src/main/java/cn/behring/fragmentnavigationmanagement/FirstFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.navigation.findNavController
 
 
 class FirstFragment : Fragment() {
@@ -21,7 +27,19 @@ class FirstFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "first fragment")
+
+                Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = CenterHorizontally) {
+                    Text(text = "first fragment")
+
+                    Button(onClick = {
+                        FirstFragmentDirections.actionFirstFragmentToSecondFragment(message = "this is a message from first fragment")
+                            .run {
+                                findNavController().navigate(this)
+                            }
+                    }) {
+                        Text(text = "go to second fragment")
+                    }
+                }
             }
         }
     }
