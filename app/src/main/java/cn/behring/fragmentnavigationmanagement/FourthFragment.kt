04878951938af786bc722696fe91a8230f56063a
behring <behring.zhao@gmail.com>
2022-12-29 00:04:27 +0800

    initialize project and create some fragments

---
+++ app/src/main/java/cn/behring/fragmentnavigationmanagement/FourthFragment.kt
@@ -0,0 +1,29 @@
+package cn.behring.fragmentnavigationmanagement
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+
+
+class FourthFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "fourth fragment")
+            }
+        }
+    }
+
+}
